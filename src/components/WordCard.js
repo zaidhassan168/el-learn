66a491e2a37b0ff455521c886064eaeeb2d874d3
Zaidulhassan168 <zaidulhassan@elgrocer.com>
2023-05-09 14:01:18 +0500

    --added wordcard

--- src/components/WordCard.js
+++ src/components/WordCard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import 'firebase/database';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -10,9 +11,6 @@ import Typography from '@mui/material/Typography';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-
 
 export default function WordCard() {
   const theme = useTheme();
@@ -23,9 +21,8 @@ export default function WordCard() {
   useEffect(() => {
     const fetchWord = async () => {
       const snapshot = await firebase.database().ref(`words/${currentIndex}`).once('value');
-      const wordt = snapshot.val();
-      setWord(wordt);
-      console.log(wordt);
+      const word = snapshot.val();
+      setWord(word);
     };
 
     fetchWord();
@@ -36,32 +33,31 @@ export default function WordCard() {
   };
 
   const handleApiCall = async () => {
+    if (!word) return;
+
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     const responseData = await response.json();
     setResponse(responseData);
-    };
+  };
 
-  if (!response) {
-    return null; // Render nothing until the word is fetched
+  if (!word) {
+    return null;
   }
 
-  const wordText = response[0].word;
-  const phonetics = response[0].phonetics;
-  const meanings = response[0].meanings;
   return (
     <Card>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Typography component="div" variant="h5">
-          hello
+            {word}
           </Typography>
-          {/* {phonetics.map((phonetic, index) => (
+          {response && response[0].phonetics.map((phonetic, index) => (
             <div key={index}>
               {phonetic.text && <Typography variant="subtitle1">{phonetic.text}</Typography>}
               {phonetic.audio && <audio controls src={phonetic.audio} />}
             </div>
-          ))} */}
-          {/* {meanings.map((meaning, index) => (
+          ))}
+          {response && response[0].meanings.map((meaning, index) => (
             <div key={index}>
               <Typography variant="h6">{meaning.partOfSpeech}</Typography>
               {meaning.definitions.map((definition, idx) => (
@@ -70,10 +66,10 @@ export default function WordCard() {
                 </Typography>
               ))}
             </div>
-          ))} */}
+          ))}
         </CardContent>
-        {/* <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous">
+        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+          <IconButton aria-label="previous" onClick={() => setCurrentIndex(currentIndex - 1)}>
             {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
           </IconButton>
           <IconButton aria-label="play/pause" onClick={handleApiCall}>
@@ -82,7 +78,7 @@ export default function WordCard() {
           <IconButton aria-label="next" onClick={handleNextClick}>
             {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
           </IconButton>
-        </Box> */}
+        </Box>
       </Box>
     </Card>
   );
