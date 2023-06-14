7c6545e052b30f1e01c137477b06c8e4eace6ea4
zaidhassan168 <zaidhassan168@outlook.com>
2023-06-15 01:40:08 +0500

    merge

--- src/components/ChaptersList.js
+++ src/components/ChaptersList.js
@@ -1,5 +1,6 @@
-// Importing necessary components and functions
-import { useState, useEffect, useRef, forwardRef } from "react";
+
+// Importing necessary components and functions from MUI and other files
+import { useState, useEffect, useRef, forwardRef } from "react"; // Importing necessary hooks from React
 import {
   Box,
   List,
@@ -50,23 +51,6 @@ import Tooltip from "@mui/material/Tooltip";
 import WordDetails from "./WordDetails";
 
 const ChaptersList = () => {
-  // State variables
-  const [isListOpen, setIsListOpen] = useState(true); // Toggle the list view
-  const [dialogOpen, setDialogOpen] = useState(false); // Control the dialog visibility
-  const [selectedWord, setSelectedWord] = useState(""); // Track the selected word
-  const [selectedChapter, setSelectedChapter] = useState(null); // Track the selected chapter
-  const [currentWordIndex, setCurrentWordIndex] = useState(0); // Track the current word index
-  const [translation, setTranslation] = useState(""); // Store the translation of a word
-  const [choices, setChoices] = useState([]); // Store the choices for the multiple-choice quiz
-  const [selectedChoice, setSelectedChoice] = useState(""); // Track the selected choice
-  const [error, setError] = useState(""); // Store the error message
-  const [isExampleOpen, setIsExampleOpen] = useState(false); // Control the example dialog visibility
-  const [chapters, setChapters] = useState([]); // Store the chapters
-  const [language, setLanguage] = useState(getSelectedLanguage()); // Track the selected language
-  const [answer, setAnswer] = useState(null); // Store the answer result
-  const containerRef = useRef(null); // Reference to the container element
-  const [isCallingExampleAPI, setIsCallingExampleAPI] = useState(false); // Track whether the dictionary example API is being called
-  const [apiResponse2, setApiResponse2] = useState(null); // Store the response from the dictionary example API
 
   // State variables for managing the component's behavior
   const [isListOpen, setIsListOpen] = useState(true); // boolean state variable to toggle the display of the chapter list
@@ -94,7 +78,6 @@ const ChaptersList = () => {
     return <Slide direction="up" ref={ref} {...props} />;
   });
 
-  // Fetch the chapters and set the chapters state
   // This useEffect hook is used to fetch the chapters and set them in the state variable
   useEffect(() => {
     getLanguage();
@@ -109,9 +92,9 @@ const ChaptersList = () => {
     if (selectedChapter) {
       const word = selectedChapter.words[currentWordIndex];
       getTranslation(word).then((data) => {
-        // Fetch translation for the selected word
+        // console.log(data);
         setTranslation(data);
-        setIsListOpen(false); // Close the choices list
+        setIsListOpen(false);
       });
     }
   }, [selectedChapter, currentWordIndex]);
@@ -121,7 +104,6 @@ const ChaptersList = () => {
     if (translation) {
       const choices = [translation];
       while (choices.length < 4) {
-        // Randomly select additional words as choices
         const randomWord =
           chapters[Math.floor(Math.random() * chapters.length)].words[
           Math.floor(Math.random() * selectedChapter.words.length)
@@ -130,21 +112,21 @@ const ChaptersList = () => {
           choices.push(randomWord);
         }
       }
-      setChoices(shuffle(choices)); // Shuffle the choices array
+      setChoices(shuffle(choices));
     }
   }, [chapters, selectedChapter, translation]);
 
   // This function toggles the display of the chapter list
   const toggleList = () => {
-    setIsListOpen((prevState) => !prevState); // Toggle the state of choices list
+    setIsListOpen((prevState) => !prevState);
   };
-
 
   // This function gets the selected language and sets it in the state variable
   const getLanguage = async () => {
     const language = await getSelectedLanguage();
-    setLanguage(language); // Set the language state
+    setLanguage(language);
   };
+
 
 
 
@@ -157,12 +139,12 @@ const ChaptersList = () => {
     setSelectedChoice("");
     setError("");
     setAnswer(false);
-
-    // Retrieve the user's unique ID and the progress for the selected chapter and language
     const user = firebase.auth().currentUser;
     const uid = user.uid;
+    console.log(uid);
+    // console.log(selectedChapter.id);
     const selectedChapterRef = firebase.database().ref("userProgress").child(uid).child(chapter.id);
-    const languageRef = selectedChapterRef.child(language);
+    const languageRef = selectedChapterRef.child(language); // Replace "language" with the selected language
 
     // Retrieve the current word index for the selected language
     languageRef.once("value", (snapshot) => {
@@ -215,21 +197,27 @@ const ChaptersList = () => {
   const handleCheckAnswer = async () => {
     if (selectedChoice.toLowerCase() === translation.toLowerCase()) {
       setError("");
+
+      // dbRef.child("learnedWords").push(selectedChapter.words[currentWordIndex]);
+      // dbRef
+      //   .child("score")
+      //   .transaction((currentScore) => (currentScore || 0) + 1);
       setAnswer(true);
-
+      // handleNextWord();
       setLanguage(await getSelectedLanguage());
-
-      // Update the user's progress by incrementing the current word index
       const user = firebase.auth().currentUser;
       const uid = user.uid;
+      console.log(uid);
+      console.log(selectedChapter);
       const selectedChapterRef = firebase.database().ref("userProgress").child(uid).child(selectedChapter.id);
-      const languageRef = selectedChapterRef.child(language);
-      languageRef.set(currentWordIndex + 1); // Store the next word index
+      console.log(language);
+      const languageRef = selectedChapterRef.child(language); // Replace "language" with the selected language
+
+      languageRef.set(currentWordIndex + 1) // Store the next word index
     } else {
       setError("Incorrect answer. Try again.");
     }
   };
-
 
   // This function is called when the "Close" button is clicked and sets the selected chapter, translation, and answer state variables to null
   const handleClose = () => {
@@ -258,10 +246,10 @@ const ChaptersList = () => {
   const handleExampleclick = async () => {
     setIsExampleOpen(true);
 
+    // setDialogWordIndex(selectedChapter.words.indexOf(word));
     try {
       setIsCallingExampleAPI(true);
-
-      // Call the dictionary API to fetch example sentences
+      console.log(selectedChapter.words[currentWordIndex]);
       const result = await callDictionaryAPI(selectedChapter.words[currentWordIndex]);
       setApiResponse2(
         await callDictionaryExampleAPI(
@@ -269,15 +257,18 @@ const ChaptersList = () => {
           result[0].translations[0].displayTarget
         )
       );
-
+      // callTextToSpeechAPI(result[0].displaySource);
       setIsCallingExampleAPI(false);
+      // setTranslatedWord(result[0].translations[0].displayTarget);
       console.log(result[0].translations);
       console.log(apiResponse2);
+      // setApiResponse(result);
+      // Update the response state variable for the next word
+      // Example: setApiResponse(result);
     } catch (error) {
       console.error(error);
     }
   };
-
 
   return (
     <Box
@@ -293,11 +284,12 @@ const ChaptersList = () => {
       // Set the ref of the Box component to the containerRef variable
       ref={containerRef}
     >
-      {/* Chapter List */}
       {isListOpen && (
         <Box
           sx={{
+            // position: "fixed",
             top: "0",
+            // left: isListOpen ? "0" : "-30%",
             zIndex: "1",
             width: "20%",
             p: 1.5,
@@ -309,7 +301,6 @@ const ChaptersList = () => {
             transition: "left 0.3s ease-in-out",
           }}
         >
-          {/* Slide animation */}
           <Slide
             // Set the direction of the slide animation to "right"
             direction="right"
@@ -348,7 +339,6 @@ const ChaptersList = () => {
                   }}
                 >
                   {/* Set the direction of the slide animation to "right" */}
-                  {/* Slide animation */}
                   <Slide direction="right" in={isListOpen} timeout={2000}>
                     <ListItemText
                       // Set the primary text of the ListItemText to the chapter id
@@ -365,8 +355,6 @@ const ChaptersList = () => {
           </Slide>
         </Box>
       )}
-
-      {/* Toggle button for Chapter List */}
 
 
       
@@ -395,10 +383,7 @@ const ChaptersList = () => {
             animation: "fadeIn 1s ease-in-out",
           }}
         >
-          {/* Chapter title */}
-          <Typography
-            style={{ marginBottom: '30px', fontWeight: 'bold', fontSize: 'larger' }}
-          >
+          <Typography style={{ marginBottom: '30px', fontWeight: 'bold', fontSize: 'larger' }}>
             {selectedChapter.id} - {selectedChapter.title}
           </Typography>         
 
@@ -431,8 +416,6 @@ const ChaptersList = () => {
             Words Learned: {currentWordIndex + 1} /{" "}
             {selectedChapter.words.length}
           </Typography>
-
-          {/* Card displaying word details */}
           <Card
             sx={{
               mb: 2,
@@ -447,7 +430,6 @@ const ChaptersList = () => {
           >
             <CardContent>
               <Grid container alignItems="center" spacing={2}>
-                {/* Button to see word details */}
                 <Grid item>
                   <Tooltip
                     title="Click to see source word details"
@@ -466,7 +448,6 @@ const ChaptersList = () => {
                     </IconButton>
                   </Tooltip>
                 </Grid>
-                {/* Displaying the source word */}
                 <Grid item xs={6}>
                   <Typography
                     variant="h5"
@@ -480,7 +461,6 @@ const ChaptersList = () => {
                     {selectedChapter.words[currentWordIndex]}
                   </Typography>
                 </Grid>
-                {/* Button to hear pronunciation */}
                 <Grid item>
                   <Tooltip
                     title="Click to hear pronunciation of Translated word"
@@ -496,7 +476,6 @@ const ChaptersList = () => {
                     </IconButton>
                   </Tooltip>
                 </Grid>
-                {/* Button to show examples */}
                 <Grid item xs={11}>
                   <Box mt={1}>
                     <Button variant="text" onClick={handleExampleclick}>
@@ -508,7 +487,6 @@ const ChaptersList = () => {
             </CardContent>
           </Card>
 
-          {/* Display translation choices */}
           {translation && (
             <FormControl
               component="fieldset"
@@ -540,7 +518,6 @@ const ChaptersList = () => {
               </Grid>
             </FormControl>
           )}
-          {/* Buttons for navigation and answer checking */}
           <div>
             <Button
               variant="contained"
@@ -572,7 +549,7 @@ const ChaptersList = () => {
           >
             Check Answer
           </Button>
-          {/* Display feedback for correct and wrong answers */}
+
           {error && (
             <Box>
               <Typography color="error" sx={{ mt: 2, textAlign: "center" }}>
@@ -615,7 +592,6 @@ const ChaptersList = () => {
           </Button>
         </Box>
       )}
-      {/* Rendered when no chapter is selected */}
       {!selectedChapter && (
         <Box
           sx={{
@@ -647,8 +623,8 @@ const ChaptersList = () => {
           </Typography>
         </Box>
       )}
-      {/* Rendered when the word details dialog is open */}
       {dialogOpen && (
+        // <WordDetails open={dialogOpen} onClose={() => setDialogOpen(false)} word={selectedWord} />
         <Dialog
           open={dialogOpen}
           onClose={handleDialogClose}
@@ -663,7 +639,6 @@ const ChaptersList = () => {
           </Button>
         </Dialog>
       )}
-      {/* Rendered when the example dialog is open */}
       <Dialog
         open={isExampleOpen}
         onClose={handleExamplesClose}
